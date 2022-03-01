366811b5d50c41f0b5ffb2d0f123d356721f3516
jayantsinghjhala <87444196+jayantsinghjhala@users.noreply.github.com>
2022-03-01 20:15:10 +0530

    Create chacematrix.R

---
+++ chacematrix.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+cacheinverse <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("Acquiring cached data>> Please Wait...")
+    return(inv)
+  }
+  Matrix_Inversion <- x$get()
+  inv <- solve(Matrix_Inversion, ...)
+  x$setinverse(inv)
+  inv
+}
